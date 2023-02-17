305662d8c1478c852a760e412e82044c028e41a4
Andy Newhouse <hi@andymnewhouse.me>
2023-02-17 13:35:45 -0600

    Remove extra timestamps

--- database/migrations/2023_02_17_192316_create_activities_table.php
+++ database/migrations/2023_02_17_192316_create_activities_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('color');
             $table->string('icon');
             $table->timestamps();
-            $table->timestamps();
         });
     }
 
